4897a4940d9201fd5810d908691b8e94c76e8e19
sunilbabu1981 <sunilbabu@gmail.com>
2018-07-11 15:16:46 -0400

    How to comment lines of code in Python
    ex002.py is the code
    snapshot002_1 is the command to run/execute this code (ex002.py)
    snapshot002_2 is the result of running the command that is displayed in snapshot002_1.

---
+++ ex002.py
@@ -0,0 +1,9 @@
+# 	A Comment, this is so that you can read this program later.
+#	Anything after the # is ignored by Python
+
+print "I could have code like this" # and the comment after is ignored
+
+#	You can also use a comment to "disable" or comment out a piece of code
+#	print "this will not print"
+
+print "But this will run"
