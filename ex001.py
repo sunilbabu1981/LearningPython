84c0402c2e8df5d848d4895c66523496cbdef8c2
sunilbabu1981 <sunilbabu@gmail.com>
2018-07-11 13:11:24 -0400

    this is the first program
    The program is simply full of print statements

---
+++ ex001.py
@@ -0,0 +1,7 @@
+# This is the first program in python as per the book "Learn Python the Hard way"
+print "Hello World!"
+print "Hello Again"
+print "I like typing this."
+print "this is fun/"
+print 'yay! Printing..'
+print "I'd much rather you 'not'"
