90dfe25ca946a4dc00d6f12fbb2967cf59c21a31
sunilbabu1981 <sunilbabu@gmail.com>
2018-07-11 16:04:11 -0400

    This exercise is about Math Functions
    ex003.py is the Code for math functions
    ex003_snapshot_01 is the command to run / execute the python program
    ex003_snapshot_02 is the result of running the ex003.py

---
+++ ex003.py
@@ -0,0 +1,25 @@
+# Exercise 3 - Numbers and Math
+
+print "I will now count my chickens"
+
+print "Hens", 25 + 30/6
+print "Roosters", 100 - 25 * 3 % 4 # apply BODMAS rule first 25 * 3 = 75 and then 75 % 4 which is 3 and at last 100 - 3 which is 97
+
+print "Now i will count the eggs"
+
+print 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
+
+print "Is it true that 3 + 2 < 5 - 7?"
+
+print 3 + 2 < 5 - 7
+
+print "what is 3 + 2?", 3 + 2
+print "what is 5 - 7?", 5 - 7
+
+print "oh thats why it's false"
+
+print "how about some more?"
+
+print "Is 5 greater than -2[5 > -2]?", 5 > -2
+print "Is 5 greater than or equal to -2[5 >= -2]?", 5 >= -2
+print "Is 5 less than or equal to -2[5 <= -2]?", 5 <= -2
