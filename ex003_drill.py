08eb497bad394648519caf271c81f2c2e3300704
sunilbabu1981 <sunilbabu@gmail.com>
2018-07-11 16:20:42 -0400

    MathFunction with Floating number
    ex003_drill.py - code to deal with fraction numbers
    ex003_snapshot_drill01 - snapshot for command to run / execute the python program above
    ex004_snapshot_drill02 - snapshot displaying results from the above command

---
+++ ex003_drill.py
@@ -0,0 +1,7 @@
+# Exercise 3 - Numbers and Math
+
+print "I will now count my chickens"
+
+print "Hens", 25 + 30.0/6
+
+print "Roosters", 100 - 25 * 3.0 % 4 # apply BODMAS rule first 25 * 3 = 75 and then 75 % 4 which is 3 and at last 100 - 3 which is 97
